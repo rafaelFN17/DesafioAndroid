88bed90ed55e319b79c8185035fc65f8074b13d2
rafaelFN17 <rafaelfranco.gu@gmail.com>
2021-05-20 02:17:40 -0300

    Atualização da ultima tela (Disntância entre dois pontos)
    Foi criado o layout da tela que vai calcular a distância entre dois pontos

--- DesafioAndroid/app/src/main/java/com/example/desafioandroid/DistanciaActivity.java
+++ DesafioAndroid/app/src/main/java/com/example/desafioandroid/DistanciaActivity.java
@@ -3,12 +3,48 @@ package com.example.desafioandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.TextView;
+import java.text.DecimalFormat;
 
 public class DistanciaActivity extends AppCompatActivity {
+
+    private EditText coordXpontoA;
+    private EditText coordYpontoA;
+    private EditText coordXpontoB;
+    private EditText coordYpontoB;
+    private TextView ResultadoDistancia;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_distancia);
+
+        coordXpontoA = findViewById(R.id.editCoordenadaXPontoA);
+        coordYpontoA = findViewById(R.id.editCoordenadaYPontoA);
+        coordXpontoB = findViewById(R.id.editCoordenadaXPontoB);
+        coordYpontoB = findViewById(R.id.editCoordenadaYPontoB);
+        ResultadoDistancia = findViewById(R.id.ResultadoDistancia);
+
+    }
+
+    public void calcularDistancia(View view){
+        try {
+            double pontoAX = Double.parseDouble(coordXpontoA.getText().toString());
+            double pontoAY = Double.parseDouble(coordYpontoA.getText().toString());
+            double pontoBX = Double.parseDouble(coordXpontoB.getText().toString());
+            double pontoBY = Double.parseDouble(coordYpontoB.getText().toString());
+
+        }catch (Exception e){
+
+        }
+    }
+
+    public void esconderTeclado(View view){
+        InputMethodManager inputManager = (InputMethodManager) getSystemService(this.INPUT_METHOD_SERVICE);
+        inputManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
     }
 }
