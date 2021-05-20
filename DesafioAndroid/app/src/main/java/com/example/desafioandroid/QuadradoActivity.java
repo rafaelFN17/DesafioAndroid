041d34409f0682e250017528144eefd5973c4ef5
rafaelFN17 <rafaelfranco.gu@gmail.com>
2021-05-20 00:03:11 -0300

    Primeiro commit com as funcionalidades tabuada e calculo de quadrado e cubo

---
+++ DesafioAndroid/app/src/main/java/com/example/desafioandroid/QuadradoActivity.java
@@ -0,0 +1,55 @@
+package com.example.desafioandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.TextView;
+import java.text.DecimalFormat;
+
+public class QuadradoActivity extends AppCompatActivity {
+
+    private EditText valorInserido;
+    private TextView Resultado;
+    private double ase;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quadrado);
+
+        valorInserido = findViewById(R.id.edtValorBase);
+        Resultado = findViewById(R.id.txtViewResultado);
+
+
+    }
+
+    public  void calcularQuadradoCubo(View view){
+        try {
+            double valor = Double.parseDouble(valorInserido.getText().toString());
+            double quadrado = valor * valor;
+            double cubo = quadrado * valor;
+            DecimalFormat df = new DecimalFormat("#,###.00");
+
+            String resultado = "";
+            resultado = "Numero" +"\n"
+                        + valor + "\n"
+                        + "Quadrado" +"\n"
+                        + df.format(quadrado) +"\n"
+                        + "Cubo"+"\n"
+                        + df.format(cubo);
+
+            Resultado.setText(resultado);
+            esconderTeclado(view);
+        }catch (Exception e){
+            valorInserido.setError("Digite um número decimal");
+            esconderTeclado(view);
+        }
+    }
+    public void esconderTeclado(View view){
+        InputMethodManager inputManager = (InputMethodManager) getSystemService(this.INPUT_METHOD_SERVICE);
+        inputManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
+    }
+}
